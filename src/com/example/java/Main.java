7a6796cdd9086ac2e37238f74b691992035e601c
Jose Juarez <jjuarez5631@students.mchenry.edu>
2017-09-09 14:59:23 -0500

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,30 @@
+package com.example.java;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte myByte = 1;
+	short myShort = 5;
+	int myInt = 250;
+	long myLong = 5500L;
+	float myFloat = 2.1f;
+	double myDouble = 56.555d;
+
+	boolean myBoolean = true;
+	char myChar = 'c';
+
+
+		System.out.println("Byte: " + myByte);
+		System.out.println("Short: " + myShort);
+		System.out.println("Int: " + myInt);
+		System.out.println("Long: " + myLong);
+		System.out.println("Float: " + myFloat);
+		System.out.println("Double: " + myDouble);
+
+		System.out.println("Boolean: " + myBoolean);
+		System.out.println("Char: " + myChar);
+
+	}
+}
